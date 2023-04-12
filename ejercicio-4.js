a56de58257caa1e636fef6eb52bc107acff409f2
JDiegoH <ajuanh@correo.udistrital.edu.co>
2023-04-12 16:08:42 -0500

    ejercicio 4

---
+++ ejercicio-4.js
@@ -0,0 +1,29 @@
+//codigo para entender los tipos de variables más usados
+
+let cadenaTexto = 'Hola mundo!';
+console.log(cadenaTexto)
+console.log(typeof(cadenaTexto))
+
+//let nombre = "Camilo";
+//cadenaTexto= "Hola!" + " Mi nombre es " + nombre
+console.log(cadenaTexto)
+
+//let edad = 23;
+//console.log(`${nombre} tiene ${edad} años`)
+//console.log(typeof(edad))
+
+let verdadero = true;
+console.log("El verdadero se define con",verdadero)
+console.log(typeof(verdadero))
+
+let color;
+console.log(`Mi color favorito es ${color}`)
+console.log(typeof(color))
+
+let nombre = "Diego";
+let apellido = "Hernández";
+let edad = 22;
+let cancion = "la cucaracha";
+let respuesta = true;
+
+console.log(`Hola! Mi nombre es ${nombre} ${apellido} y tengo ${edad} años, mi canción favorita es ${cancion} y la respuesta es ${respuesta}`)
