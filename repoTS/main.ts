d51a5233e93b79e13c5a5b94d04a921b592c06ba
JDiegoH <ajuanh@correo.udistrital.edu.co>
2023-05-08 15:14:57 -0500

    Typescript

--- repoTS/main.ts
+++ repoTS/main.ts
@@ -11,20 +11,13 @@ let a:number=6
 let verdader:boolean=true
 //Any es el tipo de dato en donde se puede meter cualquier dato
 let cualquierValor:any="Variable que recibe cualquier dato"
-
 // Constantes
-
 const PI:number=3.1416
-
 console.log(hola, a, PI)
-
 //Arrays
-
 let dias:string[]=["Lunes", "Martes", "Miercoles", "Viernes", "Sabado", "Domingo"]
-
 //DOM
 var $dias:HTMLElement=document.querySelector("#dias")
-
 dias.forEach(dia => {
     const $li = document.createElement("li")
     $li.textContent = dia
